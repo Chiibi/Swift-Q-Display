3720971cdc5873d16e305c681bb7de965260e36e
Chiibi <chiibi.jame@gmail.com>
2025-04-23 00:25:08 +0700

    chore: move env file

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth"; // Optional: if using Firebase Auth
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCCWJzF4ZR8dOd8SgEzuN_onp4odK3WK8o",
-  authDomain: "scc-queue.firebaseapp.com",
-  projectId: "scc-queue",
-  storageBucket: "scc-queue.firebasestorage.app",
-  messagingSenderId: "392786976763",
-  appId: "1:392786976763:web:444f7fbc70368831c3f55e",
-  measurementId: "G-RY97MTCN11"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
